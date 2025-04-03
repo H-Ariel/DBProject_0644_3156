318ffbe2d0e4a34713a3f178b369943f436f1a4d
ariel halili <arielhalili259@gmail.com>
2025-04-03 10:56:26 +0300

    update python script, add screenshots

--- שלב א/python-scripts/script.py
+++ שלב א/python-scripts/script.py
@@ -1,56 +1,90 @@
+# Python script to connect to PostgreSQL database and insert initial data
+
 import random
 from faker import Faker
+import psycopg2
 
 fake = Faker()
 
-# פתיחת קובץ לכתיבה
-with open('insert_data.sql', 'w') as file:
+# init connection
+print('connect...')
+conn = psycopg2.connect(
+    host='host.docker.internal', #"172.17.0.2",
+    port="5432",
+    database="postgres",
+    user="myuser",
+    password="mypassword"
+)
+cursor = conn.cursor()
 
-    # פקודות INSERT עבור טבלת Colony
-    for i in range(1, 401):
-        name = fake.company()
-        established_date = fake.date_this_century()
-        file.write(f"INSERT INTO Colony (colony_id, name, established_date) VALUES ({i}, '{name}', '{established_date}');\n")
+# Run command on SQL Server
+def insert_data(query, values=()):
+    cursor.execute(query, values)
+    conn.commit()
 
-    # פקודות INSERT עבור טבלת Crew
-    for i in range(1, 401):
-        name = fake.name()
-        role = random.choice(['Commander', 'Engineer', 'Scientist', 'Technician'])
-        joined_date = fake.date_this_century()
-        colony_id = random.randint(1, 400)
-        file.write(f"INSERT INTO Crew (crew_id, name, role, joined_date, colony_id) VALUES ({i}, '{name}', '{role}', '{joined_date}', {colony_id});\n")
 
-    # פקודות INSERT עבור טבלת Researcher
-    for i in range(1, 401):
-        name = fake.name()
-        research_field = random.choice(['AI Research', 'Radiation Studies', 'Astrobiology', 'Geology'])
-        crew_id = random.randint(1, 400)
-        file.write(f"INSERT INTO Researcher (researcher_id, name, research_field, crew_id) VALUES ({i}, '{name}', '{research_field}', {crew_id});\n")
+print('insert data')
 
-    # פקודות INSERT עבור טבלת Mission
-    for i in range(1, 401):
-        name = fake.bs()
-        description = fake.sentence()
-        start_date = fake.date_this_century()
-        colony_id = random.randint(1, 400)
-        file.write(f"INSERT INTO Mission (mission_id, name, description, start_date, colony_id) VALUES ({i}, '{name}', '{description}', '{start_date}', {colony_id});\n")
+# INSERT Colony
+print('Colony')
+for i in range(1, 401):
+    name = fake.company()
+    established_date = fake.date_this_century()
+    insert_data(f"INSERT INTO Colony (colony_id, name, established_date) VALUES ({i}, '{name}', '{established_date}');\n")
 
-    # פקודות INSERT עבור טבלת Equipment
-    for i in range(1, 401):
-        name = fake.word()
-        purchase_date = fake.date_this_century()
-        file.write(f"INSERT INTO Equipment (equipment_id, name, purchase_date) VALUES ({i}, '{name}', '{purchase_date}');\n")
+# INSERT Crew
+print('Crew')
+for i in range(1, 401):
+    name = fake.name()
+    role = random.choice(['Commander', 'Engineer', 'Scientist', 'Technician'])
+    joined_date = fake.date_this_century()
+    colony_id = random.randint(1, 400)
+    insert_data(f"INSERT INTO Crew (crew_id, name, role, joined_date, colony_id) VALUES ({i}, '{name}', '{role}', '{joined_date}', {colony_id});\n")
 
-    # פקודות INSERT עבור טבלת Infrastructure
-    for i in range(1, 401):
-        capacity = random.randint(10, 100)
-        infrastructure_type = random.choice(['Habitat', 'Power Station', 'Greenhouse', 'Research Lab'])
-        status = random.choice([1, 2, 3])  # 1=Active, 2=Inactive, 3=Under Construction
-        colony_id = random.randint(1, 400)
-        file.write(f"INSERT INTO Infrastructure (infrastructure_id, capacity, infrastructure_type, status, colony_id) VALUES ({i}, {capacity}, '{infrastructure_type}', {status}, {colony_id});\n")
+# INSERT Researcher
+# NOTE: load from CSV
+#print('Researcher')
+#for i in range(1, 401):
+#    name = fake.name()
+#    research_field = random.choice(['AI Research', 'Radiation Studies', 'Astrobiology', 'Geology'])
+#    crew_id = random.randint(1, 400)
+#    insert_data(f"INSERT INTO Researcher (researcher_id, name, research_field, crew_id) VALUES ({i}, '{name}', '{research_field}', {crew_id});\n")
 
-    # פקודות INSERT עבור טבלת Uses
-    for i in range(1, 401):
-        mission_id = random.randint(1, 400)
-        equipment_id = random.randint(1, 400)
-        file.write(f"INSERT INTO Uses (mission_id, equipment_id) VALUES ({mission_id}, {equipment_id});\n")
+# INSERT Mission
+print('Mission')
+for i in range(1, 401):
+    name = fake.bs()
+    description = fake.sentence()
+    start_date = fake.date_this_century()
+    colony_id = random.randint(1, 400)
+    insert_data(f"INSERT INTO Mission (mission_id, name, description, start_date, colony_id) VALUES ({i}, '{name}', '{description}', '{start_date}', {colony_id});\n")
+
+# INSERT Equipment
+print('Equipment')
+for i in range(1, 401):
+    name = fake.word()
+    purchase_date = fake.date_this_century()
+    insert_data(f"INSERT INTO Equipment (equipment_id, name, purchase_date) VALUES ({i}, '{name}', '{purchase_date}');\n")
+
+# INSERT Infrastructure
+print('Infrastructure')
+for i in range(1, 401):
+    capacity = random.randint(10, 100)
+    infrastructure_type = random.choice(['Habitat', 'Power Station', 'Greenhouse', 'Research Lab'])
+    status = random.choice([1, 2, 3])  # 1=Active, 2=Inactive, 3=Under Construction
+    colony_id = random.randint(1, 400)
+    insert_data(f"INSERT INTO Infrastructure (infrastructure_id, capacity, infrastructure_type, status, colony_id) VALUES ({i}, {capacity}, '{infrastructure_type}', {status}, {colony_id});\n")
+
+# INSERT Uses
+print('Uses')
+uses = set()
+while len(uses) < 400:
+    mission_id = random.randint(1, 400)
+    equipment_id = random.randint(1, 400)
+    uses.add((mission_id,equipment_id))
+
+for mission_id, equipment_id in uses:
+    insert_data(f'INSERT INTO Uses (mission_id, equipment_id) VALUES ({mission_id},{equipment_id});\n')
+
+
+print('END')
